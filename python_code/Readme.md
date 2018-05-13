117aae73660faf83d0fb0cb4993e915233f358a4
pk-python <pk.dl.python@gmail.com>
2018-05-13 12:49:52 +0530

    Initial commit

---
+++ python_code/Readme.md
@@ -0,0 +1,7 @@
+# Pure python codebase
+
+##### Sample python run command
+
+```bash
+python3 script_name.py 
+```
---
+++ python_code/helloworld.py
@@ -0,0 +1,11 @@
+class Coding():
+
+    def __init__(self):
+        """ Constructor initialization"""
+
+    def helloworld(self) -> str:
+        return 'Hello World'
+
+if __name__ == '__main__':
+    instance = Coding()
+    print (instance.helloworld())
